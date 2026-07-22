201538869c8af909948099ac5b663b40d8ab8546
jewwiid <jude.okun@gmail.com>
2026-07-23 00:00:03 +0100

    style(header): bump wordmark height 36 -> 50px

--- components/Header.tsx
+++ components/Header.tsx
@@ -45,7 +45,7 @@ export function Header() {
 
         <Logo
           href="/"
-          height={36}
+          height={50}
           className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
         />
 
